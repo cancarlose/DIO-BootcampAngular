3151cf939053fd523acdf61adf5eb27916e7c17c
carlos <carlosedlima4@gmail.com>
2024-02-07 22:54:04 -0300

    docs update TypeScript

--- 02 - TypeScript/02 - interface e comparação com o type/01 -  Interfaces.ts
+++ 02 - TypeScript/02 - interface e comparação com o type/01 -  Interfaces.ts
@@ -4,8 +4,9 @@ type robot = {
 };
 
 interface robot2 {
-  id: number;
+  id: number | string;
   name: string;
+  sayHello(): string;
 }
 
 const bot/*2*/: robot ={
@@ -13,6 +14,24 @@ const bot/*2*/: robot ={
   name: "megaman",
 };
 
+console.log(bot);
+
 // Type é mais utilizade para tipar um objeto, e Interface é mais utilazado quando trabalhamos com classe
 // Interface é um contrato, precisa seguir fielmente seus atributos.
 // Multi Tipos são aceitados
+
+class Pessoa implements robot2 {
+  id: number | string;
+  name: string;
+
+  constructor(id: number | string , name: string) {
+    this.id = id
+    this.name = name
+  }
+  sayHello(): string {
+    return "Hello";
+  }
+}
+
+const p = new Pessoa(1, "Joao");
+console.log(p.sayHello()); // Hello 
