3aab35f3d935b3fe714e00f9d4d5a14376d12718
carlos <carlosedlima4@gmail.com>
2024-02-24 14:14:48 -0300

    docs update checked-sample, create docheck events

--- Desafios/Desafio Angular/life-cycle/src/app/checked-sample/checked-sample.component.ts
+++ Desafios/Desafio Angular/life-cycle/src/app/checked-sample/checked-sample.component.ts
@@ -1,13 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  DoCheck,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-checked-sample',
   templateUrl: './checked-sample.component.html',
   styleUrls: ['./checked-sample.component.css']
 })
-export class CheckedSampleComponent implements OnInit {
+export class CheckedSampleComponent implements OnInit, DoCheck, AfterContentChecked,AfterContentInit, AfterViewChecked, AfterViewInit {
 
-  constructor() { }
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+  ngAfterViewChecked(): void {
+
+  }
+  ngAfterContentInit(): void {
+
+  }
+  ngAfterContentChecked(): void {
+
+  }
+  ngDoCheck(): void {
+
+  }
 
   ngOnInit(): void {
   }
